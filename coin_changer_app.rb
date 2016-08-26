baa6239ac5603402551af2f31dc109f1fdfd39fd
Carrison57 <calebarrison@gmail.com>
2016-08-26 12:12:51 -0400

    got name to come up

--- coin_changer_app.rb
+++ coin_changer_app.rb
@@ -1,9 +1,19 @@
 require "sinatra"
 
-get "/" do #being displayed to the server
+get "/" do #getting information from the server and pushing it the browser.
 	erb :get_name
 end
 
-post "/name" do #taking the information from the browser to the server. 
+post "/name" do #taking the information from the browser and posting it to the server. 
 	name = params[:user_name]
+	redirect "/coin_changer?user_name=" + name
 end
+
+get "/coin_changer" do
+	name = params[:user_name]
+	erb :get_change, :locals => {:your_name => name}
+end
+
+# post "/coin_changer" do
+	
+# end
